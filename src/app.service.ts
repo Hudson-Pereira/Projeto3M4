d20316694accc172db879c4937883c73f3844b65
Hudson-Pereira <hudson.o.pereira@gmail.com>
2022-01-05 22:21:43 -0300

    ajustes

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,22 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
-const listaObj = [
-  { nome: 'leo', email: 'leo@leo.com.br', idade: 24 },
-  { nome: 'Dudinha', email: 'du@da.com.br', idade: 15 },
-  { nome: 'marcio', email: 'marcio@chamma.com.br', idade: 35 },
-];
-//tirar
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Rede social online';
-  }
-
-  getLista(): string[] {
-    return lista;
-  }
-
-  getLista2(): object[] {
-    return listaObj;
   }
 }
