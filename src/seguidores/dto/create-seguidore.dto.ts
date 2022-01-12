e6157d3c64b09a6009f1e9ebfccc0b33c0d7a309
Hudson-Pereira <hudson.o.pereira@gmail.com>
2022-01-11 21:51:06 -0300

    ajustes

--- src/seguidores/dto/create-seguidore.dto.ts
+++ src/seguidores/dto/create-seguidore.dto.ts
@@ -2,7 +2,7 @@ import { IsInt, IsNotEmpty, IsString } from "class-validator";
 
 export class CreateSeguidoreDto {
   @IsNotEmpty()
-  @IsString()
-  idSeguidor: number;
+  @IsInt()
+  idsegue: number;
  
 }
--- src/seguindo/dto/create-seguindo.dto.ts
+++ src/seguindo/dto/create-seguindo.dto.ts
@@ -1,8 +1,8 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsInt, IsNotEmpty, IsString } from "class-validator";
 
 export class CreateSeguindoDto {
   @IsNotEmpty()
-  @IsString()
-  idSeguindo: number;
+  @IsInt()
+  idseguido: number;
  
 }
